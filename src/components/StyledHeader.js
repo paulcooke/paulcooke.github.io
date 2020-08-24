11f9890486fb883ada0879e17d99535719a53bb3
Paul Cooke <paulgfcooke@gmail.com>
2020-08-24 15:12:11 +0100

    added logo in top left and realigned nav bar with media queries

--- src/components/StyledHeader.js
+++ src/components/StyledHeader.js
@@ -6,29 +6,41 @@ const StyledHeaderBar = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
+  padding: 10px 20px;
     div {
       display: flex;
     }
 `
 
 const StyledHome = styled.div`
-  margin: 10px;
-  padding 8px;
+  padding: 0px 4px 8px 4px;
+  margin: 4px 0;
+  @media only screen and (max-width: 500px) {
+    padding: 0px 4px 4px 4px;
+  }
 `
 
 const StyledNavButton = styled.div`
   margin: 4px 10px;
-  padding: 7px;
+  padding: 8px;
   transition: 0.2s;
   &:hover {
     border-bottom: solid 1px #26557c;
     margin-bottom: 0px;
   }
+  @media only screen and (max-width: 500px) {
+    padding: 4px;
+  }
 `
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  color: #26557c;
+  font-size: 1.4em;
+  font-weight: bold;
+  @media only screen and (max-width: 500px) {
+    font-size: 1.2em;
+  }
 `
 
 const StyledHeader = () => {
@@ -37,7 +49,7 @@ const StyledHeader = () => {
       <StyledHeaderBar>
         <StyledHome>
           <StyledLink to="/">
-            <div>PC</div>
+            <div>&lt; pc &gt;</div>
           </StyledLink>
         </StyledHome>
         
